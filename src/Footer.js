b2fc6f7c72f826d86b6ac43338d4687e123ba90d
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-21 19:10:21 +0000

    adding footer spacing

--- src/Footer.js
+++ src/Footer.js
@@ -7,21 +7,24 @@ import { FiGithub } from 'react-icons/fi'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 
-function Footer () {
-	
-	return ( 
-		<>
-		<div className="fixed-bottom">
-		 <Navbar style={{backgroundColor:"rgb(43,48,53)", marginTop: "100px"}}>
-		<Container className="justify-content-center">
-	<Nav.Link style={{marginRight: "20px", marginLeft: "20px", color: "white"}} href="#"><SiUpwork/></Nav.Link>
-	<Nav.Link style={{marginRight: "20px", marginLeft: "20px", color: "white"}} href="#"><FaLinkedin/></Nav.Link>
-	<Nav.Link style={{marginRight: "20px", marginLeft: "20px", color: "white"}} href="#"><RiTwitterXLine/></Nav.Link>
-	<Nav.Link style={{marginRight: "20px", marginLeft: "20px", color: "white"}} href="#"><FiGithub/></Nav.Link>
-		</Container>
-		</Navbar>
-		</div>
-		</>
-	);
+function Footer() {
+	    return (
+		            <>
+		                <div className="fixed-bottom">
+		                    <Navbar style={{ backgroundColor: "rgb(43,48,53)", height: "60px" }}>
+		                        <Container className="justify-content-between align-items-center">
+		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#">123-123-1234</Nav.Link>
+		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#"><SiUpwork /></Nav.Link>
+		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#"><FaLinkedin /></Nav.Link>
+		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#"><RiTwitterXLine /></Nav.Link>
+		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#"><FiGithub /></Nav.Link>
+		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#">helloworld@outlook.com</Nav.Link>
+		                        </Container>
+		                    </Navbar>
+		                </div>
+		            </>
+		        );
 }
+
 export default Footer;
+

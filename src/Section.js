cccb91436fe49fc1ba7286fdc9857272977bc206
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-21 22:04:06 +0000

    adding responsivness

--- src/Section.js
+++ src/Section.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import photo from './currentlogo.png'
+import photo from './currentlogo.png';
 
-function Section({ title, subtitle, dark, id, color}) {
+function Section({ title, subtitle, dark, id, color }) {
 	  const sectionStyle = {
 		      display: 'flex',
-		      
+		      flexDirection: 'column', // Set flex direction to column for mobile responsiveness
 		      backgroundColor: color || (dark ? '#333' : '#fff'),
 		      border: 'solid',
-		      marginTop: '100px',
-		      marginBottom: '250px',
+		      marginTop: '50px', // Adjust margin for mobile
+		      marginBottom: '50px', // Adjust margin for mobile
 		      borderRadius: '10px',
+		      padding: '20px', // Adjust padding for mobile
 		    };
 
 	  const leftimg = {
-		      float: 'left',
-		      padding: '20px', // Adjust the padding as needed
+		      textAlign: 'center', // Center the image on mobile
+		      marginBottom: '20px', // Adjust margin for mobile
 		    };
 
 	  const rightColumnStyle = {
-		      flex: '0',
-		      padding: '20px', // Adjust the padding as needed
+		      padding: '20px',
 		    };
 
 	  const subtitleStyle = {
@@ -30,13 +30,11 @@ function Section({ title, subtitle, dark, id, color}) {
 		      <div className={`section${dark ? ' section-dark' : ''}`} style={sectionStyle}>
 		        <div className="section-content" id={id}>
 		          <div style={leftimg}>
-		  		<img  src={photo} alt="photo"/>
+		            <img src={photo} alt="photo" style={{ maxWidth: '100%', height: 'auto' }} />
 		          </div>
 		          <div style={rightColumnStyle}>
 		            <h1>{title}</h1>
-		            <p style={subtitleStyle}>
-		              {subtitle}
-		            </p>
+		            <p style={subtitleStyle}>{subtitle}</p>
 		          </div>
 		        </div>
 		      </div>

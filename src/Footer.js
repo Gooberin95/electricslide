b1be530e437791113d1883f0273c7bb2301ec7b0
Brandon Ruiz <Sober4202020@outlook.com>
2024-01-25 23:14:10 +0000

    adding none sticky footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ import Container from 'react-bootstrap/Container'
 function Footer() {
 	    return (
 		            <>
-		                <div className="fixed-bottom">
+		                
 		                    <Navbar style={{ backgroundColor: "rgb(43,48,25)", height: "60px" }}>
 		                        <Container className="justify-content-between align-items-center">
 		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#">123-123-1234</Nav.Link>
@@ -16,7 +16,6 @@ function Footer() {
 		                            <Nav.Link style={{ marginRight: "20px", marginLeft: "20px", color: "white" }} href="#">helloworld@outlook.com</Nav.Link>
 		                        </Container>
 		                    </Navbar>
-		                </div>
 		            </>
 		        );
 }
